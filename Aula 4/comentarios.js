36117b7cd9bddb40f10196e777f5bc5be8b5f57a
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2023-10-24 18:39:24 -0300

    Create comentarios.js

---
+++ Aula 4/comentarios.js
@@ -0,0 +1,15 @@
+// Uso de comentários, pi pi pi po po po
+
+console.log('Olá Mundo!'); //Console
+console.log('1...');
+//console.log('2...')
+console.log('3...');
+//console.log('4...')
+
+//Comentário de Linha
+
+/* 
+Comentário
+de
+bloco
+*/
