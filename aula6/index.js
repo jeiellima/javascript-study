787e7e13162bd057e167be195454f6d9e66ae36d
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2023-10-25 22:51:17 -0300

    Update index.js

---
+++ aula6/index.js
@@ -0,0 +1,11 @@
+let nome = 'Jeiel';
+const anoNasc = 1997;
+let nome1 = 'Ananda'
+const anoMeet = '2022'
+
+console.log(nome, 'nasceu em', anoNasc)
+console.log(nome, 'conheceu', nome1, 'em', anoMeet)
+
+let algo; //Variável declarada(vazia)
+algo = 'Alguma coisa.'; //Inicializando a variável
+console.log(algo);
