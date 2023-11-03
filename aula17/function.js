5aff382194cfaeed799a2cd198f62d0ac16ad507
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2023-11-03 16:03:05 -0300

    Create function.js

---
+++ aula17/function.js
@@ -0,0 +1,21 @@
+function calc(a, b) {
+    let result = a + b;
+    return `${a} + ${b} = ${result}`;
+}
+
+console.log(calc(6, 2));
+
+function hello(nome = String) {
+    console.log(`Bom dia ${nome}!`);
+}
+hello('Jeiel')
+
+
+function raiz(n) {
+    return n ** 0.5;
+}
+
+console.log(raiz(9))
+
+// Arrow Function
+//const raiz = n => n ** 0.5;
