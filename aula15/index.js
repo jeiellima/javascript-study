a6e12728c7e433b1c80fc4ba61deefd65fed62b6
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2023-11-03 15:23:34 -0300

    Create index.js

---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.55;
+let math = Math.floor(num1);
+let math1 = Math.ceil(num1);
+let math2 = Math.round(num1);
+console.log(math2);
+
+let arNumber = [1,2,5,7,-5,-10,100];
+
+for(let i = 0; i < arNumber.length; i++)
+console.log(Math.max(arNumber.map));
+console.log(Math.min(arNumber.map));
